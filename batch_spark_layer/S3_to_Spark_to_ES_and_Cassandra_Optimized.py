52947fd6331217dadcc1e6c9bd1824205830087a
mrubash1 <mrubash1@gmail.com>
2015-10-02 02:15:02 +0000

    updated pyspark script with increased partitions to manage 1e6 wiki pages

--- batch_spark_layer/S3_to_Spark_to_ES_and_Cassandra_Optimized.py
+++ batch_spark_layer/S3_to_Spark_to_ES_and_Cassandra_Optimized.py
@@ -16,9 +16,9 @@ from pyspark.sql import SQLContext
 
 # setting SparkContext and SQLContext
 sqlContext = SQLContext(sc)
-file_location = "s3n://rubash-commoncrawl/Wikipedia_CDX_index_results_January_2015_upload_to_s3/"
+#file_location = "s3n://rubash-commoncrawl/Wikipedia_CDX_index_results_January_2015_upload_to_s3/"
 #file_location = "s3n://rubash-commoncrawl/Wikipedia_150922_SMALL/"
-#file_location = "s3n://rubash-commoncrawl/Wikipedia_CDX_index_results_January_2015_WHOLE_DATA_SET_s3_12nodeDownload/"
+file_location = "s3n://rubash-commoncrawl/Wikipedia_CDX_index_results_January_2015_WHOLE_DATA_SET_s3_12nodeDownload/"
 
 # <codecell>
 
@@ -33,8 +33,8 @@ df_wet.printSchema()
 # <codecell>
 
 #Create an RDD of links and plaintext, with key:URL and value
-repartition_number=24
-RDD_links=df_wat.map(lambda column: (column.url,column.links)).partitionBy(repartition_number).persist(StorageLevel.MEMORY_AND_DISK_SER) #Persisted, but avoid crashing machine
+repartition_number=240
+RDD_links=df_wat.map(lambda column: (column.url,column.links)).partitionBy(repartition_number)#.persist(StorageLevel.MEMORY_AND_DISK_SER) #Persisted, but avoid crashing machine
 #Make sure all edges go both way in the RDD_links
 RDD_plaintext=df_wet.map(lambda column: (column.url,column.plaintext)).partitionBy(repartition_number).persist(StorageLevel.MEMORY_AND_DISK_SER)
 
@@ -48,20 +48,70 @@ RDD_plaintext.getNumPartitions()
 
 # <codecell>
 
+def WriteToElasticSearch_bypartition(index_name, doc_type_name, kv_iter):
+    
+    print ('SECOND TEST')
+        
+    from elasticsearch import Elasticsearch
+    #es = Elasticsearch(['172.31.2.97']) #Original 3 node cluster
+    #es= Elasticsearch(['52.89.148.224']) #dead 8 node cluster
+    es = Elasticsearch(['52.89.66.139'])
+    
+    #THIS MUST BE IN AN ITERABLE FUNCTION
+    #if it is not an iterable
+    for kv in kv_iter:
+        _id=str(kv[0])
+        body=kv[1]
+        
+        #doc = {'url': str(_id),'rank': body,} #was incorrectly assigned to rank
+        doc = {'url': str(_id),'plaintext': body,}
+        
+        
+        es.index(index=index_name,doc_type=doc_type_name, id=_id, body=doc)
+
+def test_function(index_name, doc_type_name, rdd):
+    # Write to ElasticSearch partition by partition
+    #rdd.foreachPartition(lambda kv_iter: WriteToElasticSearch_bypartition(index_name,doc_type_name,kv_iter))
+    try:
+        rdd.foreachPartition(lambda kv_iter: WriteToElasticSearch_bypartition(index_name,doc_type_name,kv_iter,))
+        print ('Data sent to Elasticsearch')
+    except:
+        print ('Data NOT sent to Elasticsearch')
+        pass
+
+#Send the plaintext to elasticsearch
+#index_name='test_no_colon'
+index_name='test_151001'
+doc_type_name='plaintext'
+#test_function(index_name,doc_type_name,RDD_plaintext)
+#test_function(index_name,doc_type_name,RDD_plaintext_repartioned)
+#took 130 seconds
+
+#Query Elasticsearch to confirm that data was sent
+from elasticsearch import Elasticsearch
+es= Elasticsearch(['52.89.66.139'])
+es.indices.refresh(index_name)
+res = es.search(index_name, body={"query": {"match_all": {}}})
+print("Got %d Entries:" % res['hits']['total'])
+
+#Unpersist RDD
+RDD_plaintext.unpersist()
+
+# <codecell>
+
 # Loads all URLs with other URL(s) link to from input file and initialize ranks of them to one.
 #IN THE FUTURE THIS COULD BE CHANGED TO HAVE ALTERED WEIGHTS
 ranks = RDD_links.map(lambda RDD_links: (RDD_links[0], 1.0))
-ranks.take(1)
+#ranks.take(1)
 #takes 11.5 seconds
 
 # <codecell>
 
 RDD_links.join(ranks)
-RDD_links.take(1)
+#RDD_links.take(1)
 
 # <codecell>
 
-RDD_plaintext.take(2)
 
 # <codecell>
 
@@ -97,6 +147,9 @@ print ("Page Rank Results:")
 ranks.take(5)
 
 #Took 7.2 seconds
+
+# <codecell>
+
 
 # <codecell>
 
@@ -142,49 +195,6 @@ if save == True:
 
 # <codecell>
 
-def WriteToElasticSearch_bypartition(index_name, doc_type_name, kv_iter):
-    
-    print ('SECOND TEST')
-        
-    from elasticsearch import Elasticsearch
-    #es = Elasticsearch(['172.31.2.97']) #Original 3 node cluster
-    #es= Elasticsearch(['52.89.148.224']) #dead 8 node cluster
-    es = Elasticsearch(['52.89.66.139'])
-    
-    #THIS MUST BE IN AN ITERABLE FUNCTION
-    #if it is not an iterable
-    for kv in kv_iter:
-        _id=str(kv[0])
-        body=kv[1]
-        
-        #doc = {'url': str(_id),'rank': body,} #was incorrectly assigned to rank
-        doc = {'url': str(_id),'plaintext': body,}
-        
-        
-        es.index(index=index_name,doc_type=doc_type_name, id=_id, body=doc)
-
-# <codecell>
-
-def test_function(index_name, doc_type_name, rdd):
-    # Write to ElasticSearch partition by partition
-    #rdd.foreachPartition(lambda kv_iter: WriteToElasticSearch_bypartition(index_name,doc_type_name,kv_iter))
-    try:
-        rdd.foreachPartition(lambda kv_iter: WriteToElasticSearch_bypartition(index_name,doc_type_name,kv_iter,))
-        print ('Data sent to Elasticsearch')
-    except:
-        print ('Data NOT sent to Elasticsearch')
-        pass
-
-# <codecell>
-
-#Send the plaintext to elasticsearch
-#index_name='test_no_colon'
-index_name='test_150929'
-doc_type_name='plaintext'
-test_function(index_name,doc_type_name,RDD_plaintext)
-#test_function(index_name,doc_type_name,RDD_plaintext_repartioned)
-
-#took 130 seconds
 
 # <codecell>
 
@@ -194,12 +204,12 @@ test_function(index_name,doc_type_name,RDD_plaintext)
 
 # <codecell>
 
-#Query Elasticsearch to confirm that data was sent
-from elasticsearch import Elasticsearch
-es= Elasticsearch(['52.89.66.139'])
-es.indices.refresh(index_name)
-res = es.search(index_name, body={"query": {"match_all": {}}})
-print("Got %d Entries:" % res['hits']['total'])
+
+# <codecell>
+
+
+# <codecell>
+
 
 # <codecell>
 
@@ -252,7 +262,7 @@ def AddToCassandra_allcountsbatch_bypartition(d_iter): #filter_missing_values=Tr
     #CASSANDRA_KEYSPACE = "wikipedia_jan_2015"
     CASSANDRA_KEYSPACE = "test"
     connection.setup(['52.89.66.139','52.89.34.7','52.89.116.45','52.89.78.4', '52.89.27.115','52.89.133.147','52.89.1.48'], CASSANDRA_KEYSPACE)
-    class url_ranks_links_21(Model):
+    class url_ranks_links_23(Model):
         #primary key is url which is dictated by the number of links
         url = columns.Text(primary_key=True)
         ranks = columns.Float()#this will be stored as a double # this is a primary key to sort on later
@@ -260,9 +270,9 @@ def AddToCassandra_allcountsbatch_bypartition(d_iter): #filter_missing_values=Tr
         def __repr__(self):
             return '%s %s' % (self.url, self.ranks)
     
-    sync_table(url_ranks_links_21)
+    sync_table(url_ranks_links_23)
     for d in d_iter:
-        url_ranks_links_21.create(**d)
+        url_ranks_links_23.create(**d)
 
        
 # Create table if it does not exist. Need to do this before submitting to Spark to avoid collisions
