b609441810b8d7555a2c8ce535da74ff51409a4f
Ubuntu <ubuntu@ip-172-31-38-36.us-west-2.compute.internal>
2015-10-06 22:44:13 +0000

    updated and cleaned git repository

--- serving_layer_elastic_cassandra/ElasticSearch_Query.py
+++
@@ -1,8 +0,0 @@
-#ElasticSearch_Query.py
-from elasticsearch import Elasticsearch
-import json
-
-es = Elasticsearch()
-#result = es.search(index="movie_db", body={'query': {'match': {'description': 'CIA'}}})
-result = es.search(index="test-31")
-print json.dumps(result, indent=2)
